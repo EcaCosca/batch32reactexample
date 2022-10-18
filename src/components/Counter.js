4100fafce31f00ce23aaa56b11495485f29aa851
EcaCosca <enriquecoscarelli@gmail.com>
2022-10-18 15:24:08 +0200

    useState Example

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,10 +3,16 @@ import React, {useState} from 'react'
 const Counter = () => {
     const [count, setCount] = useState(0)
 
+    const handleClick = () => {
+        setCount(count+1)
+    }
+
 
   return (
     <div>
         <h2>{count}</h2>
+
+        <button onClick={handleClick}>+</button>
     </div>
   )
 }
