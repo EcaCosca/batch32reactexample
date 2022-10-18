70a777f0a02b90bc19c72a8ab89cc7ca86891732
EcaCosca <enriquecoscarelli@gmail.com>
2022-10-18 15:29:53 +0200

    adding notes to counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,6 +3,9 @@ import React, {useState} from 'react'
 const Counter = () => {
     const [count, setCount] = useState(0)
     // const count = 0    The old way of writing a variable
+    // From now on whenever I use "count" it will display the current value, the same as a variable name
+    // setCount serves the porpuse of updating the count value 
+    // whatever i put between the parenthesis in useState will be the value of count in the beggining
 
     const handleClickIncrease = () => {
         setCount(count+1)
